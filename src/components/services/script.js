bd9a998ea0dec02c55271b9e9df34474852bada7
yuriirusiev <babakoshka@gmail.com>
2019-07-18 16:33:56 +0300

    added swipes

--- src/components/services/script.js
+++ src/components/services/script.js
@@ -1,4 +1,5 @@
 let slider = document.querySelector(".services__columnbox");
+let sliderWrapper = document.querySelector(".services__columnbox__wrapper");
 let sliderItems = slider.querySelectorAll(".services__columnbox__column");
 let sliderControlLeft = slider.querySelector(".services__columnbox__slider-control--left");
 let sliderControlRight = slider.querySelector(".services__columnbox__slider-control--rigth");
@@ -10,8 +11,66 @@ let items = [];
 sliderItems.forEach ((item, index) => items.push(index));
 
 sliderControlRight.onclick = () => {
-  if (window.innerWidth >= 769) {
-        
+  if (slideStep < items.length-3) {
+    slideShiftValue -= 32.5;
+    slideShift = `0 2.5% 0 ${+slideShiftValue.toFixed(1)}%`;
+    slideStep++;
+    sliderItems[0].style.margin = slideShift;
+
+  } else {
+      slideStep = 0;
+      slideShiftValue = 2.5;
+      slideShift = `0 2.5% 0 2.5%`;
+      sliderItems[0].style.margin = slideShift;
+  }
+}
+
+sliderControlLeft.onclick = () => {
+  if (slideStep === 0) {
+    slideShiftValue -= (32.5*(items.length-3));
+    slideShift = `0 2.5% 0 ${+slideShiftValue.toFixed(1)}%`;
+    slideStep = (items.length-3);
+    sliderItems[0].style.margin = slideShift;
+
+  } else if (slideStep > 0) {
+      slideShiftValue += 32.5;
+      slideShift = `0 2.5% 0 ${+slideShiftValue.toFixed(1)}%`;
+      slideStep--;
+      sliderItems[0].style.margin = slideShift;
+  }
+}
+
+//Swipe
+
+let touchstartX = 0;
+let touchendX = 0;
+
+sliderWrapper.addEventListener('touchstart', function(event) {
+    touchstartX = event.changedTouches[0].screenX;
+}, false);
+
+sliderWrapper.addEventListener('touchend', function(event) {
+    touchendX = event.changedTouches[0].screenX;
+    handleGesture();
+}, false); 
+
+function handleGesture() {
+  if (touchendX > touchstartX) { //swipe left
+    if (slideStep === 0) {
+      slideShiftValue -= (32.5*(items.length-3));
+      slideShift = `0 2.5% 0 ${+slideShiftValue.toFixed(1)}%`;
+      slideStep = (items.length-3);
+      sliderItems[0].style.margin = slideShift;
+
+    } else if (slideStep > 0) {
+          slideShiftValue += 32.5;
+          slideShift = `0 2.5% 0 ${+slideShiftValue.toFixed(1)}%`;
+          slideStep--;
+          sliderItems[0].style.margin = slideShift;
+    }
+  }
+    
+  if (touchendX < touchstartX) { //swipe right
     if (slideStep < items.length-3) {
       slideShiftValue -= 32.5;
       slideShift = `0 2.5% 0 ${+slideShiftValue.toFixed(1)}%`;
@@ -25,22 +84,4 @@ sliderControlRight.onclick = () => {
         sliderItems[0].style.margin = slideShift;
     }
   }
-}
-
-sliderControlLeft.onclick = () => {
-    if (window.innerWidth >= 769) {
-      
-      if (slideStep === 0) {
-        slideShiftValue -= (32.5*(items.length-3));
-        slideShift = `0 2.5% 0 ${+slideShiftValue.toFixed(1)}%`;
-        slideStep = (items.length-3);
-        sliderItems[0].style.margin = slideShift;
-
-      } else if (slideStep > 0) {
-            slideShiftValue += 32.5;
-            slideShift = `0 2.5% 0 ${+slideShiftValue.toFixed(1)}%`;
-            slideStep--;
-            sliderItems[0].style.margin = slideShift;
-      }
-    } 
 }
